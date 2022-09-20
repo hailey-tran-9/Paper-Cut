dd57964b315d43763a5c42db7fb3355f0d4f876a
SeanZep <skynel13@gmail.com>
2022-09-19 23:06:14 -0700

    Working on shooting

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    private Camera mainCam;
+    private Vector3 mousePos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
