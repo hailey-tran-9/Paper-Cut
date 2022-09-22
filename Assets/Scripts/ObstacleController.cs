f6ced7c394f35bb2435a58aa75c339db80380ce7
Hailey Tran <101952089+hailey-tran-9@users.noreply.github.com>
2022-09-21 19:12:29 -0700

    Revert "Merge branch 'map-design'"
    This reverts commit 782aacb3488d0b9f447ca81b2fbc4a2b4786eecd, reversing
    changes made to 75b7af003f41c0838f0d5c374029acf4414d2b9d.

--- Assets/Scripts/ObstacleController.cs
+++ Assets/Scripts/ObstacleController.cs
@@ -7,8 +7,8 @@ public class ObstacleController : MonoBehaviour
     #region Unity_functions
     void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.tag == "Obstacle") {
-            Destroy(gameObject);
+        if (other.gameObject.tag == "Bullet") {
+            Destroy(other.gameObject);
         }
     }
     #endregion
