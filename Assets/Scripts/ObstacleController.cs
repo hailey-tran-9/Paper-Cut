62a20ffd93a9d94f8cf575435ba4372afa163e94
Hailey Tran <101952089+hailey-tran-9@users.noreply.github.com>
2022-09-21 18:17:03 -0700

    Map Edit 1

---
+++ Assets/Scripts/ObstacleController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour
+{
+    #region Unity_functions
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Bullet") {
+            Destroy(other.gameObject);
+        }
+    }
+    #endregion
+}
